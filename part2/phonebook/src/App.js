6b99076aac9fbee7c0dbea4ba4ecd091ee766ce7
peanutChowder <peanutchowder@gmail.com>
2022-05-28 16:47:25 -0600

    Completed step 3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,17 +2,20 @@ import { useState } from 'react'
 
 const Names = ({people}) => (
   people.map(person => 
-    <div key={person.name}>{person.name}</div>
+    <div key={person.name}>{person.name} {person.number}</div>
   )
 )
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',}
+    { name: 'Arto Hellas',
+      number: "254-234-1532"
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const addName = (event) => {
+  const addPerson = (event) => {
     event.preventDefault()
     
     if (persons.some(person => person.name === newName)) {
@@ -20,11 +23,13 @@ const App = () => {
     } else {
       const nameObject = {
         name: newName,
+        number: newNumber
       }
 
       setPersons(persons.concat(nameObject))
     }
     setNewName("")
+    setNewNumber("")
   }
 
   const handleNameChange = (event) => {
@@ -32,13 +37,18 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
         <div>
           <button type="submit">add</button>
         </div>
