a61b46f9dcc2db99e4c64d27f530a4a2112ab5dc
Jacob Feng <peanutchowder@gmail.com>
2022-07-16 16:16:12 -0600

    Completed step 6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import Book from './components/Book'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1},
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3},
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchTerm, setSearchTerm] = useState('')
@@ -18,6 +15,15 @@ const App = () => {
     ? persons
     : persons.filter(person => person.name.toLowerCase().includes(searchTerm.toLowerCase()))
 
+  useEffect(() =>{
+    console.log('Effect')
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        console.log("Promise Fufilled")
+        setPersons(response.data)
+      })
+  }, [])
 
   return (
     <div>
