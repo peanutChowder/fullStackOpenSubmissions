2ed6a41ac02e8156034f10d5b187519fef04bed7
Jacob Feng <jfeng4@ualberta.ca>
2022-12-21 23:15:19 -0700

    Completed step 3.4

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -43,8 +43,13 @@ app.get('/api/persons/:id', (request, response) => {
     } else {
         response.status(404).end()
     }
+})
 
-    
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id != id)
+
+    response.status(204)
 })
 
 app.get('/', (request, response) => {
