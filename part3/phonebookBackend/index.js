2229ea92cd50c62bc6b36f1705d9f4fc782ab8e7
Jacob Feng <jfeng4@ualberta.ca>
2023-01-22 16:19:53 -0700

    Completed step 3.9

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -1,11 +1,16 @@
 const { response } = require('express')
 const express = require('express')
-const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
 
 app.use(express.json()) 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
+app.use(express.static('build'))
 
+// token for displaying post request body contents
 morgan.token("body", (req, res) => {
     return JSON.stringify(req.body)
 })
