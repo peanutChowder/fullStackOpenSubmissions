63412b38d7cfc3f7d79914755d8fa70453232937
Jacob Feng <jfeng4@ualberta.ca>
2023-01-22 17:09:30 -0700

    Port change

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -93,7 +93,7 @@ app.get('/', (request, response) => {
     response.send('<h1>Welcome to the phonebook</h1>')
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
