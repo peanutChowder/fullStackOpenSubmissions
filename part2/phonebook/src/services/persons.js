395ad81a20f6d8ab4a81d94009e02b92a1b160a3
Jacob Feng <jfeng4@ualberta.ca>
2023-02-05 14:53:56 -0700

    Changed port to use .env

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "https://jf-phonebook.fly.dev/api/persons"
+const baseUrl = process.env.PORT
 
 const getAll = () => {
     const request = axios.get(baseUrl)
