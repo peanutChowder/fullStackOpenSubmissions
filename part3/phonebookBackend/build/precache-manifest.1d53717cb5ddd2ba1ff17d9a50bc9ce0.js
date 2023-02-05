b847ba682c1fe5225ea679ab3223c2167a74c562
Jacob Feng <jfeng4@ualberta.ca>
2023-02-04 17:48:21 -0700

    Updated build with fixed front end baseUrl

--- part3/phonebookBackend/build/precache-manifest.1d53717cb5ddd2ba1ff17d9a50bc9ce0.js
+++ part3/phonebookBackend/build/precache-manifest.1d53717cb5ddd2ba1ff17d9a50bc9ce0.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "f9b9657cb78e3e87c250",
+    "revision": "bd6b5e48d61e40ae4632",
     "url": "/static/css/main.48da6218.chunk.css"
   },
   {
-    "revision": "f9b9657cb78e3e87c250",
-    "url": "/static/js/main.f9b9657c.chunk.js"
+    "revision": "bd6b5e48d61e40ae4632",
+    "url": "/static/js/main.bd6b5e48.chunk.js"
   },
   {
     "revision": "1da68c6515cde6875fc7",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "daf37a3f127db127bf38bab4923e1925",
+    "revision": "1368a4a4a1ebeaf05b887c4113ff2075",
     "url": "/index.html"
   }
 ];
