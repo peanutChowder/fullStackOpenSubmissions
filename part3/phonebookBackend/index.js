c6bfe1e4014d184193a415913f8867b36412ec8b
Jacob Feng <jfeng4@ualberta.ca>
2023-02-12 19:44:59 -0700

    Completed step 3.19 + removed front end build for now

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -62,7 +62,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (body.name.trim().length === 0 || body.number.trim().length === 0) {
@@ -76,10 +76,11 @@ app.post('/api/persons', (request, response) => {
         number: body.number
     })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-        
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
     })
+        .catch(error => next(error))
 })
 
 app.get('/', (request, response) => {
@@ -99,6 +100,8 @@ const errorHandler = (error, request, response, next) => {
     
     if (error.name === "CastError") {
         return response.status(400).send({error: "malformatted id"})
+    } else if (error.name === "ValidationError") {
+        return response.status(400).send({error: error.message})
     }
 
     next(error)
