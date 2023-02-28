e8cbc31ab86a195389eebf499d9d5eeffd40876a
Jacob Feng <jfeng4@ualberta.ca>
2023-02-27 22:06:36 -0700

    Completed step 4.5

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -7,7 +7,18 @@ const totalLikes = (blogs) => {
     return blogs.map(blog => blog.likes).reduce((cumLikes, currLikes) => cumLikes + currLikes)
 }
 
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((mostLikedBlog, currBlog) => {
+        console.log(mostLikedBlog)
+        if (currBlog.likes > mostLikedBlog.likes) {
+            mostLikedBlog = currBlog
+        }
+        return mostLikedBlog
+    })
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
