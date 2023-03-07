2beac6e011eeb4267adb00df9916048a1fe32ea2
Jacob Feng <jfeng4@ualberta.ca>
2023-03-06 22:33:14 -0700

    Reformatted initial blogs to be in separate .js file

--- part4/blog-list/tests/blogs-GET.test.js
+++ part4/blog-list/tests/blogs-GET.test.js
@@ -3,33 +3,8 @@ const supertest = require("supertest")
 const app = require("../app")
 const api = supertest(app)
 const Blog = require("../models/blog")
+const { initialBlogs } = require("./list-helper")
 
-const initialBlogs = [
-    {
-        title: "jack cucumber",
-        author: "woozie",
-        url: "asd.wdwr.asdasd",
-        likes: 53,
-    },
-    {
-        title: "sio laaaoo",
-        author: "mouse man",
-        url: "up.up.up",
-        likes: 1,
-    },
-    {
-        title: "woooo",
-        author: "salmon ver",
-        url: "fishes.com",
-        likes: 74
-    },
-    {
-        title: "supa dopa",
-        author: "woopty doo",
-        url: "aaaaa.ca",
-        likes: 24
-    }
-]
 
 beforeEach(async () => {
     await Blog.deleteMany({})
---
+++ part4/blog-list/tests/list-helper.js
@@ -0,0 +1,30 @@
+const initialBlogs = [
+    {
+        title: "jack cucumber",
+        author: "woozie",
+        url: "asd.wdwr.asdasd",
+        likes: 53,
+    },
+    {
+        title: "sio laaaoo",
+        author: "mouse man",
+        url: "up.up.up",
+        likes: 1,
+    },
+    {
+        title: "woooo",
+        author: "salmon ver",
+        url: "fishes.com",
+        likes: 74
+    },
+    {
+        title: "supa dopa",
+        author: "woopty doo",
+        url: "aaaaa.ca",
+        likes: 24
+    }
+]
+
+module.exports = {
+    initialBlogs
+}
