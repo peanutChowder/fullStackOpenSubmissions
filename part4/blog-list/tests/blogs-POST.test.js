cfa2728e7ad058327772df0d8546aaf06bf7ee41
Jacob Feng <jfeng4@ualberta.ca>
2023-05-07 13:40:16 -0600

    refactor to await/async instead of promises

--- part4/blog-list/tests/blogs-POST.test.js
+++ part4/blog-list/tests/blogs-POST.test.js
@@ -7,17 +7,21 @@ const { initialBlogs } = require("./list-helper")
 
 beforeEach(async () => {
     await Blog.deleteMany({})
-    const blog = new Blog(initialBlogs[3])
-    await blog.save()
 }) 
 
-test("Post request results in extra blog and contains correct body", async () => {
+test("Post request of new blog makes blog count one and contains correct body", async () => {
+    const blog = new Blog(initialBlogs[3])
+    await blog.save()
     const response = await api.get("/api/blogs")
-    const blog = response.body[0]
+    const returnedBlog = response.body[0]
 
     expect(response.body.length).toBe(1)
-    delete blog.id
-    expect(response.body[0]).toEqual(initialBlogs[3])
+    delete returnedBlog.id
+    expect(returnedBlog).toEqual(initialBlogs[3])
+})
+
+test("Post request of blog with missing 'likes' property is assigned a value of 0 by default", async () => {
+    
 })
 
 afterAll(async () => {
