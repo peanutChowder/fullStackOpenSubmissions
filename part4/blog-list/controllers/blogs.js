fbb853723215f301176651fb12063145b6eafdb2
Jacob Feng <jfeng4@ualberta.ca>
2023-06-28 22:30:55 -0600

    Added router for updating a single blog by id

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -29,5 +29,13 @@ blogsRouter.delete("/:id", async (request, response) => {
             response.status(204).end()
         })
 }) 
+
+blogsRouter.put("/:id", async (request, response) => {
+    const newBlog = request.body
+    Blog.findByIdAndUpdate(request.params.id, newBlog, {new: true})
+        .then((updatedBlog) => {
+            response.json(updatedBlog)
+        })
+})
     
 module.exports = blogsRouter
