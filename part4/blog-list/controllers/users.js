f8422a6edece9cb2ddf820673c2fc247f23ecedd
Jacob Feng <jfeng4@ualberta.ca>
2023-06-30 17:38:43 -0600

    Added status 400 rejection for POST requests that are missing username/pw or that have user/pw that are too short

--- part4/blog-list/controllers/users.js
+++ part4/blog-list/controllers/users.js
@@ -2,15 +2,20 @@ const bcrypt = require("bcrypt")
 const userRouter = require("express").Router()
 const User = require("../models/users")
 
-// userRouter.get("/", async (request, response, next) => {
-//     const users = await User.find({})
-
-//     response.json(users)
-// })
-
 userRouter.post("/", async (request, response, next) => {
     const { username, name, password } = request.body
     const saltRounds = 13
+
+    if (username == undefined || password == undefined) {
+        // const responseBody = response.json()
+        response.status(400).json({error: "Must provide username and password in request body"})
+        return
+    }
+
+    if (username.length < 3 || password.length < 3) {
+        response.status(400).json({error: "Username and Password must be at least 3 characters long."}).end()
+        return
+    }
 
     const pwHash = await bcrypt.hash(password, saltRounds)
 
