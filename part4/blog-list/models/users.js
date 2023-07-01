c7073f147e8be242a3ec1611b0a7966896337372
Jacob Feng <jfeng4@ualberta.ca>
2023-07-01 16:46:01 -0600

    Added username unique validator and removed password hash from returned object

--- part4/blog-list/models/users.js
+++ part4/blog-list/models/users.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose")
+const uniqueValidator = require("mongoose-unique-validator")
 
 const userSchema = new mongoose.Schema({
-    username: String,
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
     name: String,
     password: String
 })
@@ -9,7 +14,12 @@ const userSchema = new mongoose.Schema({
 userSchema.set("toJSON", {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        delete returnedObject.password
     }
 })
+
+userSchema.plugin(uniqueValidator)
 
 module.exports = mongoose.model("User", userSchema)
