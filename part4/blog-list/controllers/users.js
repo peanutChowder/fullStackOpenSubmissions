9e40691d7eda5dac2ac09e638c54b3b184b020de
Jacob Feng <jfeng4@ualberta.ca>
2023-07-21 19:46:58 -0600

    Added GET route for users

--- part4/blog-list/controllers/users.js
+++ part4/blog-list/controllers/users.js
@@ -2,6 +2,12 @@ const bcrypt = require("bcrypt")
 const userRouter = require("express").Router()
 const User = require("../models/users")
 
+userRouter.get("/", async (request, response) => {
+    const users = await User.find({})
+
+    response.json(users)
+})
+
 userRouter.post("/", async (request, response, next) => {
     const { username, name, password } = request.body
     const saltRounds = 13
